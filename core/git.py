52ccd29350071aa4a653d2e05e2e2766029da917
Omar Abou Mrad <omar.aboumrad@gmail.com>
2012-06-22 18:59:56 +0300

    added diff info

--- core/git.py
+++ core/git.py
@@ -9,9 +9,13 @@ class Git:
         return [os.path.dirname(e[0]) for e in os.walk(self.base) if os.path.basename(e[0]) == '.git']
 
     def log(self, repo):
-        result = check_output(['git','--git-dir','%s/.git' % repo, 'log','--pretty=format: %h|%an|%ar|%s'])
+        result = check_output(['git','--git-dir','%s/.git' % repo, 'log','--pretty=format:%h|%an|%ar|%s'])
         split_by_line = result.split('\n')
         split_by_slash = [line.split('|') for line in split_by_line]        
         return split_by_slash
 
-
+    def commit_info(self, repo, rev):
+        result = check_output(['git','--git-dir','%s/.git' % repo, 'show', rev])
+        return result 
+         
+    
--- gitwebui.py
+++ gitwebui.py
@@ -27,9 +27,17 @@ def repos():
     logs = {}
 
     for repo in repo_paths: 
-        logs[repo] = git.log(repo) 
+        logs[repo.split('/')[-1]] = git.log(repo) 
 
     return render_template('index.html', logs=logs, error=error, basedir=BASE_REPO_DIR)
+
+@app.route('/<repo_name>/<rev>')
+def commit_info(repo_name,rev):
+    git = Git(BASE_REPO_DIR)
+    repo_path = [repo for repo in git.repos() if repo.endswith(repo_name)][0]
+    diff = git.commit_info(repo_path, rev)    
+
+    return render_template('diff.html', diff=diff) 
 
 if __name__ == '__main__':
     app.run()
