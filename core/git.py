422d7d3cc1cfcb94d86b5b56ffe0f0c8f7c37f81
Omar Abou Mrad <omar.aboumrad@gmail.com>
2012-06-22 18:17:54 +0300

    refactored logic into core

---
+++ core/git.py
@@ -0,0 +1,17 @@
+import os
+from subprocess import check_output
+
+class Git:
+    def __init__(self,base):
+        self.base = base
+
+    def repos(self):
+        return [os.path.dirname(e[0]) for e in os.walk(self.base) if os.path.basename(e[0]) == '.git']
+
+    def log(self, repo):
+        result = check_output(['git','--git-dir','%s/.git' % repo, 'log','--pretty=format: %h|%an|%ar|%s'])
+        split_by_line = result.split('\n')
+        split_by_slash = [line.split('|') for line in split_by_line]        
+        return split_by_slash
+
+
--- gitwebui.py
+++ gitwebui.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from subprocess import check_output
 from flask import Flask, render_template, flash
+from core.git import Git
 
 DEBUG = True
 
@@ -19,17 +19,15 @@ if len(sys.argv) > 1:
 @app.route('/')
 def repos():
     error = None
-    repo_paths = [os.path.dirname(e[0]) for e in os.walk(BASE_REPO_DIR) if os.path.basename(e[0]) == '.git']
+    git = Git(BASE_REPO_DIR)    
+    repo_paths = git.repos()
     if not repo_paths:
         error = 'No repositories found!'
 
     logs = {}
 
     for repo in repo_paths: 
-        result = check_output(['git','--git-dir','%s/.git' % repo, 'log','--pretty=format: %h|%an|%ar|%s'])
-        split_by_line = result.split('\n')
-        split_by_slash = [line.split('|') for line in split_by_line]        
-        logs[repo.split('/')[-1]] = split_by_slash
+        logs[repo] = git.log(repo) 
 
     return render_template('index.html', logs=logs, error=error, basedir=BASE_REPO_DIR)
 
