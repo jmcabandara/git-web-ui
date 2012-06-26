8b517b78fb5fe149be6d5e0c425d535600178302
Omar Abou Mrad <xterm@wind.(none)>
2012-06-26 14:29:48 +0300

    separated pages

--- gitwebui.py
+++ gitwebui.py
@@ -19,26 +19,34 @@ git = Git(BASE_REPO_DIR)
 repo_paths = git.repos()
 
 # Views
-
 @app.route('/')
 def repos():
     error = None
     if not repo_paths:
         error = 'No repositories found!'
 
-    logs = {}
+    repos = {}
 
-    for repo in repo_paths: 
-        logs[repo.split('/')[-1]] = git.log(repo) 
+    for repo in repo_paths:
+        repos[repo.split('/')[-1]] = repo
 
-    return render_template('index.html', logs=logs, error=error, basedir=BASE_REPO_DIR)
+    return render_template('index.html', repos=repos, error=error, basedir=BASE_REPO_DIR)
+
+
+@app.route('/<repo_name>')
+def commits(repo_name):
+    error = None
+
+    logs =  [(repo_name,git.log(x)) for x in repo_paths if x.endswith(repo_name)][0]
+
+    return render_template('repo.html', logs=logs, error=error, basedir=BASE_REPO_DIR)
 
 @app.route('/<repo_name>/<rev>')
 def commit_info(repo_name,rev):
     repo_path = filter(lambda repo: repo.endswith(repo_name), repo_paths)[0]
     commit = git.commit_info(repo_path, rev)    
 
-    return render_template('diff.html', commit=commit) 
+    return render_template('diff.html', commit=commit, repo=repo_name, rev=rev) 
 
 if __name__ == '__main__':
     app.run()
